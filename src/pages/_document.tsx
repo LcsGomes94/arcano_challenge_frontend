450304fefe71df749bf587006a8e602334bab66f
Lucas Gomes <lcsdev94@gmail.com>
2023-05-03 08:40:58 -0300

    inicializado e finalizado

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        <title>Arcano Challenge</title>
+      </Head>
+      <body className={`bg-slate-800 text-slate-300 font-sourcesanspro`}>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
